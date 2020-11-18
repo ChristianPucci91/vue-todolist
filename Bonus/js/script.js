70e2a71a64b86f648c29a6066d7f7bda1e1ad3a8
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-18 19:32:55 +0100

    Esercizio base finito. Alcuni bonus finiti, in lavorazione per altri bonus

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -1,7 +1,9 @@
-// /Features principali:
-// un input permette all’utente di scrivere una “cosa da fare”;
-// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
-// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+// BONUS AFFRONTATI
+// -1- casi limite di inserimento dati da utente: se valore è nullo o stringa vuota si verifica un errore
+// -2- se l'utente inserisce più di 50 caratteri o inserisce una parola già inserita si verifica une errore
+// -3- Bonus 1- inserita la casistica per inviare i dati direttamente con il tasto enter anzichè cliccare sul bottone di invio
+//-4- IN LAVORAZIONE bonus 2- click con icona "check" alternativa all'icona "x" di elimina e barra semplicemente la parola nella lista anzichè cancellarla.
+//-5- IN LAVORAZIONE bonus 3- quando l'utente clicca sull'icona elimina farà uscire un box di conferma se magari ho cliccato per sbaglio con dei buttons dove se dirò di si la confermerà la cancellazione altrimenti tornerà al flusso di partenza.
 
 var app = new Vue({
   el: "#app",
@@ -10,12 +12,13 @@ var app = new Vue({
       "Fare colazione",
       "Allenarsi",
     ],
-    deleteBox:"hide",
+    isActive: false,
     img:"img/todowhite.png",
     value:null // valore di quello che inserisce l'utente
   },
 
   methods: { // funzione inserimento con tasto ENTER
+
     enterValue: function () {
 
       if ((this.value == null) || (this.value == 0 )) {
@@ -30,15 +33,13 @@ var app = new Vue({
         this.toDoList.push(this.value);
         this.value = '';
       }
-
     },
+
     removeList: function (index) {
-      if (this.deleteBox = "hide") {
-        this.deleteBox = "show"
-      }
       this.toDoList.splice(index,1)
     },
-
+    signedList: function (index) {
+      this.isActive = !this.isActive;
+    }
   }
-
 })
