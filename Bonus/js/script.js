cc1ac6cbe0bba643e39a67fc39812daa472cdd4f
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-18 17:03:28 +0100

    code cleaning.

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -12,7 +12,6 @@ var app = new Vue({
     ],
     img:"img/todowhite.png",
     value:null // valore di quello che inserisce l'utente
-
   },
 
   methods: { // funzione inserimento con tasto ENTER
@@ -31,20 +30,6 @@ var app = new Vue({
         this.value = '';
       }
 
-    },
-    addValue: function () { // funzione di aggiunta value nell'array e vari casi limite di inserimento dati
-      if (this.value == null) {
-        alert("ERRORE: Devi inserire qualcosa");
-      }else if (this.value.length > 50) {
-        alert("ERRORE: Non puoi inserire più di 50 caratteri");
-        this.value = '';
-      }else if (this.toDoList.includes(this.value)) {
-        alert("ERRORE: Hai già inserito questo appunto");
-        this.value = '';
-      }else {
-        this.toDoList.push(this.value);
-        this.value = '';
-      }
     },
     removeList: function (index) {
       this.toDoList.splice(index,1)
