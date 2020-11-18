3f382915b9655dba56f6b4273c2029541a33ec89
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-18 16:16:40 +0100

    Css style update

---
+++ Bonus/js/script.js
@@ -0,0 +1,36 @@
+// /Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    toDoList: [
+      "Fare colazione",
+      "Allenarsi",
+    ],
+    img:"img/todowhite.png",
+    value:null // valore di quello che inserisce l'utente
+
+  },
+  methods: {
+    addValue: function () {
+      if (this.value == null) {
+        alert("ERRORE: Devi inserire qualcosa");
+      }
+      else if (this.value.length > 50) {
+        alert("ERRORE: Non puoi inserire più di 50 caratteri");
+      }else {
+        this.toDoList.push(this.value);
+        this.value = '';
+        console.log(this.value);
+      }
+    },
+    removeList: function (index) {
+      this.toDoList.splice(index,1)
+      console.log(this.toDoList[index]);
+    }
+  }
+
+})
